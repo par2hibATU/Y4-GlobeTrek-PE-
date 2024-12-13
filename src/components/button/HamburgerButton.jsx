82eec65c6fd5f6ce9ba3372d9a034f66a7b7d546
par2hibATU <parthibbdcox@gmail.com>
2024-12-13 05:14:41 +0000

    smaller changes

--- src/components/button/HamburgerButton.jsx
+++ src/components/button/HamburgerButton.jsx
@@ -1,12 +1,37 @@
 import "./hamburgerButton.css";
-import React, { useState } from "react";
+import React, { useState, useNavigate, handleMapClick } from "react";
 import Hamburger from "hamburger-react";
+import { FloatButton } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAddressBook, faMessage, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+
 
 export const HamburgerButton = () => {
-  const [open, setOpen] = useState(false);
-  return <div>
-    <Hamburger />
-  </div>;
+  const navigate = useNavigate();
+
+  const handleMapClick = () => {
+    navigate("/map"); // Navigate to the map page
+  };
+  return (
+    <div>
+
+      <FloatButton.Group
+        icon={<FontAwesomeIcon icon={faPlus} />}
+        trigger="click"
+      >
+        <FloatButton
+          icon={<FontAwesomeIcon icon={faMessage} />}
+          tooltip="Contact us"
+        />
+        <FloatButton
+          icon={<FontAwesomeIcon icon={faAddressBook} />}
+          tooltip="Your Account"
+          onClick={handleMapClick}
+        />
+      </FloatButton.Group>
+    </div>
+  );
 };
 
 export default HamburgerButton;
