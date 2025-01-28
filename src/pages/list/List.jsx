eb34274158974b392da2c7fdb61f1121c2b84483
par2hibATU <parthibbdcox@gmail.com>
2025-01-28 11:11:19 +0000

    Search result box is added on the hotel list and little bit of styling also added on top of that. Price range is about to set

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -5,6 +5,7 @@ import Footer from '../../components/footer/Footer'
 import Subscribe from '../mailList/Subscribe';
 import { useLocation } from 'react-router-dom';
 import { format } from 'date-fns';
+import { DateRange } from 'react-date-range';
 
 
 
@@ -12,6 +13,7 @@ export const List = () => {
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination)
   const [date, setDate] = useState(location.state.date)
+  const [openDate, setOpenDate] = useState(false)
   const [options, setOptions] = useState(location.state.options)
 
 
@@ -28,7 +30,33 @@ export const List = () => {
           </div>
           <div className="lsItem">
             <label>Check-in-date</label>
-            <span>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+            <span onClick={()=>setOpenDate(!openDate)}>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+            {openDate && ( <DateRange onChange={(item) => setDate([item.selection])} 
+            minDate={new Date()} 
+            ranges={date}/>)}
+          </div>
+          <div className="lsItem">
+            <label>Options</label>
+            <div className="lsOptionItem">
+              <span className="lsOptionText">Min price <small>per night</small></span>
+              <input type='number' className='lsOptionInput'/>
+            </div>
+            <div className="lsOptionItem">
+              <span className="lsOptionText">Max price <small>per night</small></span>
+              <input type='number' className='lsOptionInput'/>
+            </div>
+            <div className="lsOptionItem">
+              <span className="lsOptionText">Adult</span>
+              <input type='number' className='lsOptionInput'/>
+            </div>
+            <div className="lsOptionItem">
+              <span className="lsOptionText">Children</span>
+              <input type='number' className='lsOptionInput'/>
+            </div>
+            <div className="lsOptionItem">
+              <span className="lsOptionText">room</span>
+              <input type='number' className='lsOptionInput'/>
+            </div>
           </div>
         </div>
         <div className="listResult"></div>
